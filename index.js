08d5b99e97b15c7f4b06bf7883df0323fd580db4
Stephen Ochieng <materiamystic@gmail.com>
2024-03-24 17:58:34 +0300

    Adds required variables

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+let num1;
+let num2 = 2;
+let num3;
+let num4 = 6;
+
+//multiply solution
+let multiply = 62;
+num1 = multiply / num2;
+
+//random solution
+let random = Math.ceil(Math.random() * 10);
+
+//mod solution
+let mod = 4;
+num3 = num4 * random + mod;
+
+//max solution
+let max = Math.max(14, 2, 20, 18);
